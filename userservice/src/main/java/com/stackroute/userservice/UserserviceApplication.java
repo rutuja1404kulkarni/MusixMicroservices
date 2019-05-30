b99496e8109527fbc71fd748313613147aef2a66
= <=>
2019-05-30 11:09:01 +0530

    userservice application.yml added and errors solved

--- userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @EnableEurekaClient
 @SpringBootApplication
 public class UserserviceApplication {
 
-
 	public static void main(String[] args)
 	{
-
 		SpringApplication.run(UserserviceApplication.class, args);
-		System.out.println("*******************");
 	}
 
 }
