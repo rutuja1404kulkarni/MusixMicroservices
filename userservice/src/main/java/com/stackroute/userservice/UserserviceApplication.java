324840991774cc31b1b6881a23a008809b898602
= <=>
2019-05-30 10:30:13 +0530

    not required files deleted

--- userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
@@ -2,14 +2,7 @@ package com.stackroute.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-=======
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
->>>>>>> 9c7079737d21f517dde977e3938ea921b4b20261
-
 
 @EnableEurekaClient
 @SpringBootApplication
